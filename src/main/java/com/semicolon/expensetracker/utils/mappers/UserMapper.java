b61136b4ced084d638c45b87807c9c6e9913cb7f
Oladayo234 <oladayoolubunmi20@gmail.com>
2026-03-30 14:09:15 +0100

    third commit

---
+++ src/main/java/com/semicolon/expensetracker/utils/mappers/UserMapper.java
@@ -0,0 +1,40 @@
+package com.semicolon.expensetracker.utils.mappers;
+
+import com.semicolon.expensetracker.data.models.User;
+import com.semicolon.expensetracker.dtos.request.RegisterUserRequest;
+import com.semicolon.expensetracker.dtos.response.LoginResponse;
+import com.semicolon.expensetracker.dtos.response.RegisterUserResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(RegisterUserRequest request) {
+        User user = new User();
+        user.setName(request.getName());
+        user.setUsername(request.getUserName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setCurrencyPreference(request.getCurrencyPreference());
+        return user;
+    }
+
+    public RegisterUserResponse toRegisterResponse(User user) {
+        RegisterUserResponse response = new RegisterUserResponse();
+        response.setEmail(user.getEmail());
+        response.setName(user.getName());
+        response.setUserName(user.getUsername());
+        return response;
+    }
+
+    public LoginResponse toLoginResponse(User user, String token) {
+        LoginResponse response = new LoginResponse();
+        response.setId(user.getId().toString());
+        response.setName(user.getName());
+        response.setUserName(user.getUsername());
+        response.setToken(token);
+        response.setMessage("Login successful");
+        return response;
+    }
+}
